9f375beec9d1f166520575ed502e1ef90aa971ec
Tan <nhattan2012@gmail.com>
2018-09-04 15:24:03 +0800

    get currency components out of Amount component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-const App = () => <Amount />;
-
-const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
-const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
-
-class Amount extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
 
@@ -14,29 +9,44 @@ class Amount extends Component {
   }
 
   onIncrement = () => {
-    this.setState(state => ({amount: state.amount = state.amount + 1}));
-  }
+    this.setState(state => ({ amount: state.amount + 1 }));
+  };
 
   onDecrement = () => {
-    this.setState(state => ({amount: state.amount = state.amount - 1}));
-  }
+    this.setState(state => ({ amount: state.amount - 1 }));
+  };
+
   render() {
     return (
       <div>
-        <span>US Dollar: {this.state.amount} </span>
+        <Amount
+          amount={this.state.amount}
+          onIncrement={this.onIncrement}
+          onDecrement={this.onDecrement}
+        />
 
-        <button type="button" onClick={this.onIncrement}>
-          +
-        </button>
-        <button type="button" onClick={this.onDecrement}>
-          -
-        </button>
-
-        <Euro amount={this.state.amount}/>
-        <Pound amount={this.state.amount}/>
+        <Euro amount={this.state.amount} />
+        <Pound amount={this.state.amount} />
       </div>
-    )
+    );
   }
 }
+
+const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
+
+const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
+
+const Amount = ({ amount, onIncrement, onDecrement }) => (
+  <div>
+    <span>US Dollar: {amount} </span>
+
+    <button type="button" onClick={onIncrement}>
+      +
+    </button>
+    <button type="button" onClick={onDecrement}>
+      -
+    </button>
+  </div>
+);
 
 export default App;
