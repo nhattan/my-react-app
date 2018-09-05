6e0b206a7631928d213aa7aefcd5dfe9481b025d
Tan <nhattan2012@gmail.com>
2018-09-05 14:48:19 +0800

    use Higher-Order Component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,49 @@
 import React, { Component } from 'react';
 
-const App = () => (
-  <Amount
-    render_converter={amount => (
-      <div>
-        <h1>My Currency Converter</h1>
-        <Pound amount={amount} />
-        <Euro amount={amount} />
-      </div>
-    )}
-  />
-);
-class Amount extends Component {
-  constructor(props) {
-    super(props);
+const App = () => <CurrenciesWithAmount />;
 
-    this.state = {
-      amount: 0,
+const withAmount = currencyComponents =>
+  class Amount extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        amount: 0,
+      };
+    }
+
+    onIncrement = () => {
+      this.setState(state => ({ amount: state.amount + 1 }));
     };
-  }
 
-  onIncrement = () => {
-    this.setState(state => ({ amount: state.amount + 1 }));
+    onDecrement = () => {
+      this.setState(state => ({ amount: state.amount - 1 }));
+    };
+
+    render() {
+      return (
+        <div>
+          <span>US Dollar: {this.state.amount} </span>
+
+          <button type="button" onClick={this.onIncrement}>
+            +
+          </button>
+          <button type="button" onClick={this.onDecrement}>
+            -
+          </button>
+
+          {currencyComponents.map(CurrencyComponent => (
+            <CurrencyComponent amount={this.state.amount} />
+          ))}
+        </div>
+      );
+    }
   };
-
-  onDecrement = () => {
-    this.setState(state => ({ amount: state.amount - 1 }));
-  };
-
-  render() {
-    return (
-      <div>
-        <span>US Dollar: {this.state.amount} </span>
-
-        <button type="button" onClick={this.onIncrement}>
-          +
-        </button>
-        <button type="button" onClick={this.onDecrement}>
-          -
-        </button>
-
-        {this.props.render_converter(this.state.amount)}
-      </div>
-    );
-  }
-}
 
 const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
 
 const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
+
+const CurrenciesWithAmount = withAmount([Euro, Pound]);
+
 export default App;
